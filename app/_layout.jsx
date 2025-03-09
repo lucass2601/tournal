f2812ade631edb90d7d9fd71a520266b718b950a
lucass2601 <lucas.fischer2002@t-online.de>
2025-03-09 23:06:54 +0100

    style: add fonts & general theme

---
+++ app/_layout.jsx
@@ -0,0 +1,43 @@
+import {
+  useFonts,
+  Manrope_400Regular,
+  Manrope_500Medium,
+  Manrope_600SemiBold,
+  Manrope_700Bold,
+} from "@expo-google-fonts/manrope";
+
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Manrope_400Regular,
+    Manrope_500Medium,
+    Manrope_600SemiBold,
+    Manrope_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
+}
---
+++ theme/theme.js
@@ -0,0 +1,197 @@
+export const colors = {
+  // Primary
+  "primary-100": "#F1EDFD",
+  "primary-200": "#E2DAFB",
+  "primary-300": "#B9AAE7",
+  "primary-400": "#A991F3",
+  "primary-500": "#8D6CEF",
+  "primary-600": "#7047EB",
+
+  // Secondary
+  "secondary-100": "#E7EEFF",
+  "secondary-200": "#D0DDFF",
+  "secondary-300": "#A1BBFF",
+  "secondary-400": "#7199FF",
+  "secondary-500": "#4277FF",
+  "secondary-600": "#1355FF",
+
+  // Warning
+  "warning-100": "#FFFCF1",
+  "warning-200": "#FFF9E2",
+  "warning-300": "#FFF2C5",
+  "warning-400": "#FFECA8",
+  "warning-500": "#FFE58B",
+  "warning-600": "#FFDF6E",
+
+  // Error
+  "error-100": "#FEEAEF",
+  "error-200": "#FDD4DF",
+  "error-300": "#FBA9BF",
+  "error-400": "#FA7E9F",
+  "error-500": "#F8537F",
+  "error-600": "#F6285F",
+
+  // Success
+  "success-100": "#F0F9F8",
+  "success-200": "#D0EDE8",
+  "success-300": "#A0DBD1",
+  "success-400": "#71C9BA",
+  "success-500": "#41B7A3",
+  "success-600": "#12A58C",
+
+  // Greyscale
+  "grey-50": "#F7F9FC",
+  "grey-100": "#E3E4E8",
+  "grey-200": "#C1C4CD",
+  "grey-300": "#9BA0AF",
+  "grey-400": "#83899B",
+  "grey-500": "#6A7187",
+  "grey-600": "#515973",
+  "grey-700": "#38415F",
+  "grey-800": "#1E2848",
+  "grey-900": "#061237",
+
+  // Additional
+  white: "#FFFFFF",
+  grape: "#7214FF",
+  "blue-light": "#00B3FF",
+};
+
+export const typography = {
+  fontFamilyRegular: "Manrope_400Regular",
+  fontFamilyMedium: "Manrope_500Medium",
+  fontFamilySemiBold: "Manrope_600SemiBold",
+  fontFamilyBold: "Manrope_700Bold",
+
+  headings: {
+    h1: {
+      fontSize: 48,
+      lineHeight: 58, // ~48 * 1.2
+      fontFamily: "Manrope_700Bold",
+    },
+    h2: {
+      fontSize: 40,
+      lineHeight: 48, // ~40 * 1.2
+      fontFamily: "Manrope_700Bold",
+    },
+    h3: {
+      fontSize: 28,
+      lineHeight: 34, // ~28 * 1.2
+      fontFamily: "Manrope_700Bold",
+    },
+    h4: {
+      fontSize: 24,
+      lineHeight: 29, // ~24 * 1.2
+      fontFamily: "Manrope_700Bold",
+    },
+    h5: {
+      fontSize: 20,
+      lineHeight: 24, // ~20 * 1.2
+      fontFamily: "Manrope_700Bold",
+    },
+    h6: {
+      fontSize: 16,
+      lineHeight: 19, // ~16 * 1.2
+      fontFamily: "Manrope_700Bold",
+    },
+  },
+
+  body: {
+    xlarge: {
+      bold: {
+        fontSize: 18,
+        lineHeight: 27,
+        fontFamily: "Manrope_700Bold",
+      },
+      semibold: {
+        fontSize: 18,
+        lineHeight: 27,
+        fontFamily: "Manrope_600SemiBold",
+      },
+      medium: {
+        fontSize: 18,
+        lineHeight: 27,
+        fontFamily: "Manrope_500Medium",
+      },
+      regular: {
+        fontSize: 18,
+        lineHeight: 27,
+        fontFamily: "Manrope_400Regular",
+      },
+    },
+
+    large: {
+      bold: {
+        fontSize: 16,
+        lineHeight: 26, // 160% of 16 => 25.6 ~ 26
+        fontFamily: "Manrope_700Bold",
+      },
+      semibold: {
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: "Manrope_600SemiBold",
+      },
+      medium: {
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: "Manrope_500Medium",
+      },
+      regular: {
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: "Manrope_400Regular",
+      },
+    },
+
+    medium: {
+      bold: {
+        fontSize: 14,
+        lineHeight: 23, // 165% of 14 => 23.1 ~ 23
+        fontFamily: "Manrope_700Bold",
+      },
+      semibold: {
+        fontSize: 14,
+        lineHeight: 23,
+        fontFamily: "Manrope_600SemiBold",
+      },
+      medium: {
+        fontSize: 14,
+        lineHeight: 23,
+        fontFamily: "Manrope_500Medium",
+      },
+      regular: {
+        fontSize: 14,
+        lineHeight: 23,
+        fontFamily: "Manrope_400Regular",
+      },
+    },
+
+    small: {
+      bold: {
+        fontSize: 12,
+        lineHeight: 20, // 170% of 12 => 20.4 ~ 20
+        fontFamily: "Manrope_700Bold",
+      },
+      semibold: {
+        fontSize: 12,
+        lineHeight: 20,
+        fontFamily: "Manrope_600SemiBold",
+      },
+      medium: {
+        fontSize: 12,
+        lineHeight: 20,
+        fontFamily: "Manrope_500Medium",
+      },
+      regular: {
+        fontSize: 12,
+        lineHeight: 20,
+        fontFamily: "Manrope_400Regular",
+      },
+    },
+  },
+};
+
+export const theme = {
+  colors,
+  typography,
+};
