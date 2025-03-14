64ac22e39c78f021cf08a56d9d7140c89a343eab
lucass2601 <lucas.fischer2002@t-online.de>
2025-03-14 23:19:53 +0100

    add ScreenContainer component as container wrapper

---
+++ components/layouts/ScreenContainer.jsx
@@ -0,0 +1,23 @@
+import { View, StyleSheet } from "react-native";
+
+const ScreenContainer = ({ children }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>{children}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingVertical: 24,
+  },
+});
+
+export default ScreenContainer;
